ed438a0568c8760e31f799ac601e902ec6b36272
prathmesh <prathmeshkholgade1990@gmail.com>
2025-02-20 18:11:56 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
